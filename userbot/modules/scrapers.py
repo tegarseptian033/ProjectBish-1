94bde236bc7790ef7afa8f7c1562401c31da7ad9
daenghunter <65704433+daenghunter@users.noreply.github.com>
2020-06-30 06:49:38 +0800

    scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -291,7 +291,7 @@ async def text_to_speech(query):
         os.remove("k.mp3")
         if BOTLOG:
             await query.client.send_message(
-                BOTLOG_CHATID, "Text to Speech executed successfully !")
+                BOTLOG_CHATID, "Copyright ® Daeng Server Bot !")
         await query.delete()
 
 
