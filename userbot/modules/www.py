8f532d66756d566ae00719970f6edcb9f19b4e41
tegarseptian033 <67510617+tegarseptian033@users.noreply.github.com>
2020-06-30 19:28:31 +0800

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -66,10 +66,10 @@ async def neardc(event):
 async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
     start = datetime.now()
-    await pong.edit("`Seponv!`")
+    await pong.edit("`Sepong!`")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit("`Sepong!\n%sms`" % (duration))
+    await pong.edit("`Croot!\n%sms`" % (duration))
 
 
 CMD_HELP.update({
