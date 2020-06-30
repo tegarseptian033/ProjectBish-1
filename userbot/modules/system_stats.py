219dfc7a632ecce8d891fe50f04ed949cef09ebf
tegarseptian033 <67510617+tegarseptian033@users.noreply.github.com>
2020-06-30 14:13:29 +0800

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -136,6 +136,7 @@ async def amireallyalive(alive):
              f"🐍 `Python    :` v{python_version()}\n"
              f"⚙️ `Telethon  :` v{version.__version__}\n"
              f"👤 `User      :` {DEFAULTUSER}\n"
+             f"♥️ `Username  :` @Nakano Miku\n"
              f"====================================\n")
     await bot.send_file(alive.chat_id, logo, caption=output)
     await alive.delete()
