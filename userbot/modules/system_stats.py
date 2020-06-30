b6b4d11299bbb3406a4a1069b68040d1b0966a57
tegarseptian033 <67510617+tegarseptian033@users.noreply.github.com>
2020-06-30 18:04:07 +0800

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -133,12 +133,12 @@ async def amireallyalive(alive):
     logo = ALIVE_LOGO
     output = (f"`Project is Running With My Master♥️ \n"
              f"COPYRIGHT (C) 2020\n"
-             f"┏━━━━━━━━━━━━━━━━━━━━━━━━\n"
-             f"┣[ 🐍 `Python    :` v{python_version()}\n"
-             f"┣[ ⚙️ `Telethon :` v{version.__version__}\n"
-             f"┣[ 👤 `User        :` {DEFAULTUSER}\n"
-             f"┣[  👽  Username :@Nakano Miku\n"
-             f"┗━━━━━━━━━━━━━━━━━━━━━━━━\n")
+             f"•••••••••••••••••••••••••••••••••••••••••••••••••\n"
+             f"🐍 `Python    :` v{python_version()}\n"
+             f"⚙️ `Telethon  :` v{version.__version__}\n"
+             f"👤 `User      :` {DEFAULTUSER}\n"
+             f"♥️ `Username    :`@Nakano Miku\n"
+             f"•••••••••••••••••••••••••••••••••••••••••••••••••\n")
     await bot.send_file(alive.chat_id, logo, caption=output)
     await alive.delete()
 
