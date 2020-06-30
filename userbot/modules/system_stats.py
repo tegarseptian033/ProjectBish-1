0258b5706be74611e2db01ad566cbe7d61591638
tegarseptian033 <67510617+tegarseptian033@users.noreply.github.com>
2020-06-30 15:08:46 +0800

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -131,13 +131,14 @@ async def pipcheck(pip):
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     logo = ALIVE_LOGO
-    output = (f"`Projects is running on My Master
-COPYRIGHT (C) 2020`\n"
+`\n"
+    output = (f"`Projects is running on My Master \n"
              f"┏━━━━━━━━━━━━━━━━━━━━━━━━\n"
              f"┣[ 🐍 Python    :` v{python_version()}\n"
              f"┣[ ⚙️ Telethon  :` v{version.__version__}\n"
              f"┣[ 👤 User      :` {DEFAULTUSER}\n"
              f"┣[ ♥️ UserName  :@Nakano Miku\n"
+             f"┣[ COPYRIGHT (C) 2020
              f"┗━━━━━━━━━━━━━━━━━━━━━━━━\n")
     await bot.send_file(alive.chat_id, logo, caption=output)
     await alive.delete()
